06ae87e21457c4ed714b22235b8c0d73ddfa8fe4
AluMati <AluMati@PC15>
2025-03-25 08:36:13 +0100

    Funcionalidad de suma añadida

---
+++ CALCULADORA/src/CALCU/Calculadora.java
@@ -0,0 +1,17 @@
+package CALCU;
+
+public class Calculadora {
+
+	public int sumar(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		
+		System.out.println("Resultado de la suma: " +
+				calc.sumar(5, 3));
+
+	}
+
+}
